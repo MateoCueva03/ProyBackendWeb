84dbfd12019dd2ec740d69b8cecc2e77654e7356
unknown <nicolasmce99@gmail.com>
2022-08-03 18:33:19 -0500

    Sprint1

---
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
+
+use Illuminate\Contracts\Encryption\Encrypter;
+use Illuminate\Contracts\Foundation\Application;
+
+class VerifyCsrfToken extends Middleware
+{
+        // protected $urlFron = env('APP_URL');
+
+        protected $except = [];
+
+        public function __construct(Application $app, Encrypter $encrypter)
+        {
+            parent::__construct($app, $encrypter);
+    
+            $this->except[] = "{$this->app->environment('APP_URL')}/api/*";
+        }
+    
+    
+}
